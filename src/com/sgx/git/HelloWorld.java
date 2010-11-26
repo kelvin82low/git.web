8d53cf4f5fa188aa4c57caf5120f4d60ab600aeb
kelvin <kelvin.low@ufinity.com>
2010-11-26 17:02:40 +0800

    print "Hello World" greeting message

---
+++ src/com/sgx/git/HelloWorld.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright Information
+ * 
+ * (c) 2010, Ufinity Pte Ltd. All rights reserved.
+ * 315 Alexandra Road #05-01A Sime Darby Business Centre Singapore 159944
+ * 
+ * This document is protected by copyright. No part of this document may be 
+ * reproduced in any form by any means without prior written authorization of 
+ * Ufinity and its licensors, if any.
+ */
+package com.sgx.git;
+
+/**
+ * @author Kelvin Low
+ *
+ */
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!!!");
+    }
+}
